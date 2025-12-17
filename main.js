db206e5db1b1a2880d67dbfb348c9cb68352188e
sqsoft19-web <sqsoft19@gmail.com>
2025-12-17 12:32:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["CREATIVE STUDIO", "ENTERTAINMENT TECHNOLOGY","DIGITAL GAMING SOLUTIONS","GAME STUDIO EXCELLENCE","GAMING INNOVATION HUB", "PROFESSIONAL GAME DEVELOPERS"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
